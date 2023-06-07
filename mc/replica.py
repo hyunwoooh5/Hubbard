a6e8bc21c1c45dcac92853e0046f587d26b946aa
Hyunwoo Oh <hyunwooh@umd.edu>
2023-06-07 10:24:54 -0400

    faster replica exchange and add tracking data

--- mc/replica.py
+++ mc/replica.py
@@ -4,44 +4,42 @@ import numpy as np
 
 from mc import metropolis
 
+
 class ReplicaExchange:
     def __init__(self, action, x0, key, Chain=metropolis.Chain, max_hbar=10., Nreplicas=30):
         self.Nreplicas = Nreplicas
 
-        loghbars = np.linspace(0,np.log(max_hbar),Nreplicas)
-        hbars = np.exp(loghbars)
+        loghbars = jnp.linspace(0, jnp.log(max_hbar), Nreplicas)
+        hbars = jnp.exp(loghbars)
         key, self.key = jax.random.split(key, 2)
         keys = jax.random.split(key, Nreplicas)
-        self.replicas = [Chain(action, x0, key, temperature=hbar) for hbar,key in zip(hbars,keys)]
+        self.replicas = [Chain(action, x0, key, temperature=hbar)
+                         for hbar, key in zip(hbars, keys)]
+
+        for replica in self.replicas:
+            replica._track = [False]
 
         def _propose(key):
-            kswap, key = jax.random.split(key, 2)
-            kswapi, kswapj = jax.random.split(kswap, 2)
-            i=jax.random.randint(kswapi, shape=(), minval=0, maxval=self.Nreplicas)
-            j=jax.random.randint(kswapj, shape=(), minval=0, maxval=self.Nreplicas)
-            
-            return i, j, key
-
-        def _propose2(key):
-            kacc, key = jax.random.split(key, 2)
+            kswap, kacc, key = jax.random.split(key, 3)
+            i, j = jax.random.randint(kswap, shape=(
+                [2]), minval=0, maxval=self.Nreplicas)
             x = jax.random.uniform(kacc)
-            return x
+            return i, j, x, key
 
-        self._propose=jax.jit(_propose)
-        self._propose2=jax.jit(_propose2)
+        self._propose = jax.jit(_propose)
 
     def step(self, N=1):
-        for n in range(N):
-            for replica in self.replicas:
-                replica.step()
-            self.exchange()
+        for replica in self.replicas:
+            replica.step(N=N)
+            replica._track = replica._track[-100:]
+        self.exchange()
         self.x = self.replicas[0].x
 
     def exchange(self):
         if self.Nreplicas == 1:
             return
-        for _ in range(self.Nreplicas):
-            i, j, self.key = self._propose(self.key)
+        for _ in range(self.Nreplicas):  # change to jax loop?
+            i, j, rand, self.key = self._propose(self.key)
             if i == j:
                 continue
             hbari, hbarj = self.replicas[i].temperature, self.replicas[j].temperature
@@ -49,8 +47,13 @@ class ReplicaExchange:
             S = Si/hbari + Sj/hbarj
             Sp = Sj/hbari + Si/hbarj
             Sdiff = Sp - S
-            if Sdiff < 0 or self._propose2(self.key) < np.exp(-Sdiff):
-                self._swap(i,j)
+            if Sdiff < 0 or rand < jnp.exp(-Sdiff):
+                self._swap(i, j)
+                self.replicas[i]._track.append(True)
+                self.replicas[j]._track.append(True)
+            else:
+                self.replicas[i]._track.append(False)
+                self.replicas[j]._track.append(False)
 
     def _swap(self, i, j):
         temperaturei, temperaturej = self.replicas[i].temperature, self.replicas[j].temperature
@@ -67,6 +70,4 @@ class ReplicaExchange:
             yield self.replicas[0].x
 
     def acceptance_rate(self):
-        # TODO pass on information about exchanges
-        return np.mean([r.acceptance_rate() for r in self.replicas])
-
+        return [np.mean([r.acceptance_rate() for r in self.replicas]), [sum(r._track)/len(r._track) for r in self.replicas]]
